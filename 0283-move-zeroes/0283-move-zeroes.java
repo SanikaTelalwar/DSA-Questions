09f99c4d402fe15f91a99fc96031c1afd0b40833
Sanika Telalwar <telalwarsanika@gmail.com>
2026-06-22 20:12:24 +0530

    Update 0283-move-zeroes.java

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,3 +1,4 @@
+//Brute force approach 
 class Solution {
     public void moveZeroes(int[] nums) {
       ArrayList<Integer> temp=new ArrayList<>();
@@ -16,6 +17,35 @@ class Solution {
       for(int i=temp.size();i<nums.length;i++)
       {
         nums[i]=0;
+      }
+    }
+}
+
+//optimal approach 
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+      int j=-1;
+      for(int i=0;i<nums.length;i++)
+      {
+        if(nums[i]==0)
+        {
+            j=i;
+            break;
+        }
+      }
+      if(j!=-1)
+      {
+        for(int i=j+1;i<nums.length;i++)
+        {
+          if(nums[i]!=0)
+          {
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            j++;
+          }
+        }
       }
     }
 }
