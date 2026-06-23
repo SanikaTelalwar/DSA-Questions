6daee48d397e50ae002cd069a06cc8010fd9d7bb
Sanika Telalwar <telalwarsanika@gmail.com>
2026-06-23 22:06:09 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,3 +1,4 @@
+//BRUTE FORCE APPROACH
 class Solution {
     public int[] twoSum(int[] nums, int target) {
        for(int i=0;i<nums.length;i++)
@@ -17,3 +18,24 @@ class Solution {
        return new int[]{};
     }
 }
+
+//BETTER APPROACH 
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       HashMap<Integer,Integer> map=new HashMap<>();
+       for(int i=0;i<nums.length;i++)
+       {
+          int value=target-nums[i];
+          if(map.containsKey(value))
+          {
+            return new int[] { map.get(value), i };
+          }
+          map.put(nums[i],i);
+       }
+       return new int[]{};
+    }
+}
+
+//OPTIMAL APPROACH
+
