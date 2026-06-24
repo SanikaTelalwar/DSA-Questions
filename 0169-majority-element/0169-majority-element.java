912a8e2d2d982085e3f7d5ca36aebc1b689e3bc5
Sanika Telalwar <telalwarsanika@gmail.com>
2026-06-24 20:45:25 +0530

    Update 0169-majority-element.java

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,3 +1,5 @@
+//BRUTE FORCE APPROACH 
+
 class Solution {
     public int majorityElement(int[] nums) {
       for(int i=0;i<nums.length;i++)
@@ -19,3 +21,25 @@ class Solution {
       return -1;
     }
 }
+
+//BETTER APPROACH (Hashing hashmap is used )
+
+class Solution {
+    public int majorityElement(int[] nums) {
+      HashMap<Integer,Integer> map=new HashMap<>();
+      for(int i=0;i<nums.length;i++)
+      {
+        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+      }
+      for(int i=0;i<nums.length;i++)
+      {
+        if(map.get(nums[i])>nums.length/2)
+        {
+            return nums[i];
+        }
+      }
+      return -1;
+    }
+}
+
+//OPTIMAL APPROACH 
