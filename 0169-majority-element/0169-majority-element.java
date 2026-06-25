fab1700ae8289334b418798b315ab547522977c6
Sanika Telalwar <telalwarsanika@gmail.com>
2026-06-25 17:32:39 +0530

    Update 0169-majority-element.java

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -42,4 +42,40 @@ class Solution {
     }
 }
 
-//OPTIMAL APPROACH 
+//OPTIMAL APPROACH (Moore's voting algorithm)
+
+class Solution {
+    public int majorityElement(int[] nums) {
+     int el=0;
+     int count=0;
+     for(int i=0;i<nums.length;i++)
+     {
+        if(count==0)
+        {
+            count=1;
+            el=nums[i];
+        }
+        else if(nums[i]==el)
+        {
+            count++;
+        }
+        else
+        {
+            count--;
+        }
+     }
+     int count1=0;
+     for(int i=0;i<nums.length;i++)
+     {
+        if(nums[i]==el)
+        {
+            count1++;
+        }
+        if(count1>nums.length/2)
+        {
+            return el;
+        }
+     }
+     return -1;
+    }
+}
