ad459e275676bec372e2e54ddec93859c98a359b
Sanika Telalwar <telalwarsanika@gmail.com>
2026-06-25 21:45:18 +0530

    Update 2149-rearrange-array-elements-by-sign.java

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,3 +1,4 @@
+//BRUTE FORCE APPROACH 
 class Solution {
     public int[] rearrangeArray(int[] nums) {
     ArrayList<Integer> positive=new ArrayList<>();
@@ -20,5 +21,29 @@ class Solution {
    }
     return nums;
         
+    }
+}
+
+//OPTIMAL APPROACH 
+
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+    int[] result=new int[nums.length];
+    int posIndex=0;
+    int negIndex=1;
+    for(int i=0;i<nums.length;i++)
+    {
+        if(nums[i]>0)
+        {
+            result[posIndex]=nums[i];
+            posIndex+=2;
+        }
+        else
+        {
+            result[negIndex]=nums[i];
+            negIndex+=2;
+        }
+    }
+    return result;
     }
 }
