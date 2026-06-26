fcffc52cd162ed3e92761f9394e23594b8d550da
Sanika Telalwar <telalwarsanika@gmail.com>
2026-06-26 20:17:23 +0530

    Time: 3 ms (100%), Space: 175.7 MB (15.79%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,31 +1,3 @@
-//BRUTE FORCE APPROACH 
-class Solution {
-    public int[] rearrangeArray(int[] nums) {
-    ArrayList<Integer> positive=new ArrayList<>();
-    ArrayList<Integer> negative=new ArrayList<>();
-    for(int i=0;i<nums.length;i++)
-    {
-        if(nums[i]>0)
-        {
-            positive.add(nums[i]);
-        }
-        else
-        {
-            negative.add(nums[i]);
-        }
-    }
-   for(int i=0;i<nums.length/2;i++)
-   {
-      nums[2*i]=positive.get(i);
-      nums[2*i+1]=negative.get(i);
-   }
-    return nums;
-        
-    }
-}
-
-//OPTIMAL APPROACH 
-
 class Solution {
     public int[] rearrangeArray(int[] nums) {
     int[] result=new int[nums.length];
