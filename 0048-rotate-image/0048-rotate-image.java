83768021b22a99297985108d51b9d0618fe27715
Sanika Telalwar <telalwarsanika@gmail.com>
2026-06-30 22:27:30 +0530

    Update 0048-rotate-image.java

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,3 +1,4 @@
+//BRUTE FORCE APPROACH 
 class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
@@ -18,5 +19,35 @@ class Solution {
         }
 
         
+    }
+}
+
+//OPTIMAL APPROACH 
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            int left=0;
+            int right=n-1;
+            while(left<right)
+            {
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+            }
+        }
     }
 }
