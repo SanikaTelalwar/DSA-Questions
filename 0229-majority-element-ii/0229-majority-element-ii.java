d26812103192a0eb1363a6e681ae607745dbfd7c
Sanika Telalwar <telalwarsanika@gmail.com>
2026-07-03 18:18:47 +0530

    Update 0229-majority-element-ii.java

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -49,3 +49,71 @@ class Solution {
 
 //OPTIMAL APPROACH 
 
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int element1 = 0;
+        int element2 = 0;
+
+        int count1 = 0;
+        int count2 = 0;
+
+        // Find candidates
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(element1 == nums[i])
+            {
+                count1++;
+            }
+            else if(element2 == nums[i])
+            {
+                count2++;
+            }
+            else if(count1 == 0)
+            {
+                element1 = nums[i];
+                count1 = 1;
+            }
+            else if(count2 == 0)
+            {
+                element2 = nums[i];
+                count2 = 1;
+            }
+            else
+            {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Verify candidates
+        count1 = 0;
+        count2 = 0;
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] == element1)
+            {
+                count1++;
+            }
+            else if(nums[i] == element2)
+            {
+                count2++;
+            }
+        }
+
+        if(count1 > nums.length / 3)
+        {
+            list.add(element1);
+        }
+
+        if(count2 > nums.length / 3)
+        {
+            list.add(element2);
+        }
+
+        return list;
+    }
+}
