a2b4b93fd030370979b91ff37925b08c5a62ed43
Sanika Telalwar <telalwarsanika@gmail.com>
2026-07-04 16:47:45 +0530

    Update 0152-maximum-product-subarray.java

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,3 +1,5 @@
+//BRUTE FORCE APPROACH 
+
 class Solution {
     public int maxProduct(int[] nums) {
         int maxProduct=Integer.MIN_VALUE;
@@ -11,6 +13,33 @@ class Solution {
             }
         }
         return maxProduct;
+        
+    }
+}
+
+//OPTIMAL APPROACH 
+
+class Solution {
+    public int maxProduct(int[] nums) {
+       int maxProduct=Integer.MIN_VALUE;
+       int prefix=1;
+       int suffix=1;
+       for(int i=0;i<nums.length;i++)
+       {
+        if(prefix==0)
+        {
+            prefix=1;
+        }
+        if(suffix==0)
+        {
+            suffix=1;
+        }
+        prefix*=nums[i];
+        suffix*=nums[nums.length-i-1];
+        maxProduct=Math.max(maxProduct,Math.max(prefix,suffix));
+
+       }
+       return maxProduct;
         
     }
 }
